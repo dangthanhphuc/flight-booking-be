ed583585cd5842259948c3a6ef24547aa06763d3
dangthanhphuc <dangthanhphuc2512@gmail.com>
2024-08-06 02:48:52 +0700

    The first commit

---
+++ src/main/java/com/example/flight_booking_be/FlightBookingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.flight_booking_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingBeApplication.class, args);
+	}
+
+}
